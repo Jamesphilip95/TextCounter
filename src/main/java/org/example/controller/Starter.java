3757bb985c55de4d05e5153820df37e51535ae23
james <jphilip@spartaglobal.com>
2020-10-08 13:54:48 +0100

    architechure added

---
+++ src/main/java/org/example/controller/Starter.java
@@ -0,0 +1,14 @@
+package org.example.controller;
+
+/**
+ * Hello world!
+ *
+ */
+public class Starter
+{
+    public static void main( String[] args )
+    {
+        CounterManager counterManager = new CounterManager();
+        counterManager.run();
+    }
+}
